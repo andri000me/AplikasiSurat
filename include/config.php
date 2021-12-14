363de7294040ffefb671bd07a5ce9b9104501a64
HanielObedChristyawan <haniel.obed@si.ukdw.ac.id>
2021-12-14 16:29:12 +0700

    super duper baru sekali

--- include/config.php
+++ include/config.php
@@ -2,4 +2,4 @@
 $host     = "localhost";    // Nama host
 $username = "root";         // Username database
 $password = "";   // Password database
-$database = "ams";   // Nama database
+$database = "percobaan";   // Nama database
